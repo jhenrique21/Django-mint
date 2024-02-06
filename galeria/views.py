1aae9c09a28d535df23b7cf02df95eee8979cb36
João Henrique <joaokocssis@gmail.com>
2024-02-06 11:34:50 -0300

    galeria

--- galeria/views.py
+++ galeria/views.py
@@ -4,8 +4,3 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "galeria/index.html")
-"""
-def sauda(request, name):
-    return render(request, "pedidos/sauda.html",{
-        "name": name.capitalize()
-    })"""
