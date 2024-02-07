5d211bc461a0d4da77b0efc9d38ccd2e3c782da8
João Henrique <joaokocssis@gmail.com>
2024-02-07 09:32:12 -0300

    migrations

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('index/', views.index, name="index"),
     path('imagem/', views.imagem, name="imagem")
 ]
