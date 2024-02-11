4c2eff3f5b66667e66624f22bf331d9c2d6e3c4a
joaoKocssis <joaokocssis@gmail.com>
2024-02-11 08:57:15 -0300

    teste

--- galeria/admin.py
+++ galeria/admin.py
@@ -7,5 +7,6 @@ class ListaFotografias(admin.ModelAdmin):
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
     list_filter = ("categoria",)
+    list_per_page = 10
 
 admin.site.register(Fotografia, ListaFotografias)
